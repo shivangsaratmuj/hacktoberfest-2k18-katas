77e6ec6f93b22682aa013c809f3e733b9c3c0b14
Shivang Sarat <shivang.sarat@asort.com>
2018-10-04 01:03:53 +0530

    Next Power of 2.

--- src/nextPowerOfTwo.js
+++ src/nextPowerOfTwo.js
@@ -1,3 +1,14 @@
-export const nextPowerOfTwo = () => {
+export const nextPowerOfTwo = (num) => {
   // TO IMPLEMENT IN ANOTHER PR
+  var x = 1, y = 1, count = 0;
+  while(y === 1) {
+  	if (x < num) {
+  		x = x * 2;
+  		count ++
+  	}
+  	else {
+  		y = 0;
+  	}
+  }
+  return x;
 };
